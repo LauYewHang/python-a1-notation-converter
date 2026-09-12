f4853a2f46bbf0a02ffc7fa00ad1c56e4fb949d1
Sunshine <yewhang2004@gmail.com>
2026-09-12 17:41:34 +0800

    Implemented addition and subtraction for A1Notation. Added string representation and printable string representation for A1Notation.

--- src/a1_notation_converter/A1Notation.py
+++ src/a1_notation_converter/A1Notation.py
@@ -1,13 +1,15 @@
+from typing import overload
+
 from a1_notation_converter.a1_converter import *
 
 # A1Notation class
 class A1Notation:
     def __init__(self, a1_notation : str):
         try:
-            columnRowDict = a1_to_column_row(a1_notation)
+            column_row_dict = a1_to_column_row(a1_notation)
             self._a1_notation = a1_notation
-            self._column = columnRowDict["column"]
-            self._row = columnRowDict["row"]
+            self._column = column_row_dict["column"]
+            self._row = column_row_dict["row"]
         except TypeError as e:
             raise TypeError(
                 f"The type of argument 'a1_notation' of class instantiation 'A1Notation()' needs to be type 'str'.\n"
@@ -42,7 +44,7 @@ class A1Notation:
     @a1_notation.setter
     def a1_notation(self, value : str):
         try:
-            columnRowDict = a1_to_column_row(value)
+            column_row_dict = a1_to_column_row(value)
         except TypeError as e:
             raise TypeError(
                 f"The type of attribute 'a1_notation' of class 'A1Notation' needs to be type 'str'.\n"
@@ -55,8 +57,8 @@ class A1Notation:
             )
 
         self._a1_notation = value
-        self._column = columnRowDict["column"]
-        self._row = columnRowDict["row"]
+        self._column = column_row_dict["column"]
+        self._row = column_row_dict["row"]
 
     @property
     def column(self):
@@ -97,3 +99,66 @@ class A1Notation:
             )
 
         self._row = value
+
+    @overload
+    def __repr__(self): ...
+
+    def __repr__(self):
+        return f"A1Notation('{self.a1_notation}')"
+
+    @overload
+    def __str__(self): ...
+
+    def __str__(self):
+        return self.a1_notation
+
+    def __A1Notation_arithmetic(self, operation, other):
+        if (isinstance(other, A1Notation)):
+            try:
+                other_column = other.column if operation == "addition" else -(other.column)
+                other_row = other.row if operation == "addition" else -(other.row)
+            except Exception as e:
+                raise Exception(f"An unexpected exception occured: {e}")
+        elif (type(other) is dict and all(attribute in other.keys() for attribute in ["column", "row"])):
+            if (type(other["column"]) is not int or type(other["row"]) is not int):
+                raise TypeError(
+                    f"The type of value of keys 'column' and 'row' in the provided operand for A1Notation {operation} needs to be type 'int'.\n"
+                    f"Current received type of key 'column': {type(self.column)}.\n"
+                    f"Current received type of key 'row': {type(self.row)}."
+                )
+            try:
+                other_column = other["column"] if operation == "addition" else -(other["column"])
+                other_row = other["row"] if operation == "addition" else -(other["row"])
+            except Exception as e:
+                raise Exception(f"An unexpected exception occured: {e}")
+        elif ((type(other) is list or type(other) is tuple) and len(other) >= 2):
+            if (type(other[0]) is not int or type(other[1]) is not int):
+                raise TypeError(
+                    f"The type of values in the provided {type(other)} operand for A1Notation {operation} needs to be type 'int'.\n"
+                    f"Current received type of {type(other)}[0]: {other[0]}.\n"
+                    f"Current received type of {type(other)}[1]: {other[1]}.\n"
+                )
+            try:
+                other_column = other[0] if operation == "addition" else -(other[0])
+                other_row = other[1] if operation == "addition" else -(other[1])
+            except Exception as e:
+                raise Exception(f"An unexpected exception occured: {e}")
+        else:
+            raise TypeError(
+                f"The operand with A1Notation {operation} needs to be type 'A1Notation', 'ColumnRowDict', 'list', or 'tuple'.\n"
+                f"Current receive type of operand: {type(other)}"
+            )
+
+        return A1Notation.from_column_row(column = self.column + other_column, row = self.row + other_row)
+
+    @overload
+    def __add__(self, other): ...
+
+    def __add__(self, other):
+        return self.__A1Notation_arithmetic(operation = "addition", other = other)
+
+    @overload
+    def __sub__(self, other): ...
+
+    def __sub__(self, other):
+        return self.__A1Notation_arithmetic(operation = "subtraction", other = other)
